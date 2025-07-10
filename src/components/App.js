69d0b86d4eb1718ce29e66d65fa94df99c202eac
itsmeshivay0 <h80272655@gmail.com>
2025-07-10 15:18:14 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
 
-const App = () => {
+function App() {
+  const projects = [
+    {
+      name: "AI Assistant",
+      description: "An AI that helps people with their daily tasks."
+    },
+    {
+      name: "E-learning Platform",
+      description: "A platform where students can learn coding interactively."
+    },
+    {
+      name: "3D Model Viewer",
+      description: "A web app to visualize and animate 3D models."
+    }
+  ];
+
   return (
-    <div id="main"></div>
-  )
+    <div className="ns-wrapper">
+      {projects.map((project, index) => (
+        <div key={index}>
+          <h1 data-ns-test="project-name">{project.name}</h1>
+          <h6 data-ns-test="project-description">{project.description}</h6>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-
-export default App;
+export default App;
